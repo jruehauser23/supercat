78eadbb1520ae8d011eea23848a5dd4dd82b409e
jruehauser23 <163670750+jruehauser23@users.noreply.github.com>
2024-04-28 12:51:14 +0300

    Update link.js

--- js/link.js
+++ js/link.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   $("a").click(function(){
-    $("#link*").attr("href", "https://referencemen.co/ktVmDV?c=0089k-M0hoLPQPbbf00d2c52322570");
+    $("#reflink*").attr("href", "https://referencemen.co/ktVmDV?c=0089k-M0hoLPQPbbf00d2c52322570");
   });
 });
